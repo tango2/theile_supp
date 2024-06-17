a83ff50941754ea5da606964c61308ba26cbf0da
Tim Tangherlini <tango@berkeley.edu>
2024-06-17 08:35:59 -0700

    fixed interactive graphs

---
+++ assets/html/figure_02a/styles.js
@@ -0,0 +1,321 @@
+var styles = [ {
+  "format_version" : "1.0",
+  "generated_by" : "cytoscape-3.10.2",
+  "target_cytoscapejs_version" : "~2.1",
+  "title" : "Sample1",
+  "style" : [ {
+    "selector" : "node",
+    "css" : {
+      "text-opacity" : 1.0,
+      "height" : 25.0,
+      "text-valign" : "center",
+      "text-halign" : "center",
+      "border-opacity" : 1.0,
+      "font-size" : 10,
+      "background-opacity" : 1.0,
+      "font-family" : "Dialog",
+      "font-weight" : "normal",
+      "border-width" : 3.0,
+      "background-color" : "rgb(127,205,187)",
+      "color" : "rgb(51,51,51)",
+      "shape" : "ellipse",
+      "border-color" : "rgb(0,0,0)",
+      "width" : 25.0,
+      "content" : "data(label)"
+    }
+  }, {
+    "selector" : "node[sub = '1_2_5']",
+    "css" : {
+      "background-color" : "rgb(155,88,165)"
+    }
+  }, {
+    "selector" : "node[sub = '1_2_6']",
+    "css" : {
+      "background-color" : "rgb(199,61,149)"
+    }
+  }, {
+    "selector" : "node[sub = '1_2_3']",
+    "css" : {
+      "background-color" : "rgb(161,104,100)"
+    }
+  }, {
+    "selector" : "node[sub = '1_2_4']",
+    "css" : {
+      "background-color" : "rgb(123,111,169)"
+    }
+  }, {
+    "selector" : "node[sub = '1_1_2']",
+    "css" : {
+      "background-color" : "rgb(101,134,74)"
+    }
+  }, {
+    "selector" : "node[sub = '1_2_1']",
+    "css" : {
+      "background-color" : "rgb(175,109,28)"
+    }
+  }, {
+    "selector" : "node[sub = '1_2_2']",
+    "css" : {
+      "background-color" : "rgb(200,98,32)"
+    }
+  }, {
+    "selector" : "node[sub = '1_1_1']",
+    "css" : {
+      "background-color" : "rgb(27,158,119)"
+    }
+  }, {
+    "selector" : "node[sub = '2_1_3']",
+    "css" : {
+      "background-color" : "rgb(116,152,42)"
+    }
+  }, {
+    "selector" : "node[sub = '2_2_2']",
+    "css" : {
+      "background-color" : "rgb(177,127,25)"
+    }
+  }, {
+    "selector" : "node[sub = '2_1_4']",
+    "css" : {
+      "background-color" : "rgb(138,167,22)"
+    }
+  }, {
+    "selector" : "node[sub = '2_2_3']",
+    "css" : {
+      "background-color" : "rgb(152,114,45)"
+    }
+  }, {
+    "selector" : "node[sub = '2_1_5']",
+    "css" : {
+      "background-color" : "rgb(187,169,11)"
+    }
+  }, {
+    "selector" : "node[sub = '2_2_4']",
+    "css" : {
+      "background-color" : "rgb(127,108,74)"
+    }
+  }, {
+    "selector" : "node[sub = '2_1_6']",
+    "css" : {
+      "background-color" : "rgb(226,168,4)"
+    }
+  }, {
+    "selector" : "node[sub = '2_2_5']",
+    "css" : {
+      "background-color" : "rgb(102,102,102)"
+    }
+  }, {
+    "selector" : "node[sub = '2_1_1']",
+    "css" : {
+      "background-color" : "rgb(217,55,126)"
+    }
+  }, {
+    "selector" : "node[sub = '2_1_2']",
+    "css" : {
+      "background-color" : "rgb(167,104,84)"
+    }
+  }, {
+    "selector" : "node[sub = '2_2_1']",
+    "css" : {
+      "background-color" : "rgb(202,147,14)"
+    }
+  }, {
+    "selector" : "node[story_class = '1_1']",
+    "css" : {
+      "border-color" : "rgb(128,255,0)"
+    }
+  }, {
+    "selector" : "node[story_class = '1_2']",
+    "css" : {
+      "border-color" : "rgb(255,191,0)"
+    }
+  }, {
+    "selector" : "node[story_class = '2_1']",
+    "css" : {
+      "border-color" : "rgb(128,0,255)"
+    }
+  }, {
+    "selector" : "node[story_class = '2_2']",
+    "css" : {
+      "border-color" : "rgb(255,0,0)"
+    }
+  }, {
+    "selector" : "node[bipartite = 0]",
+    "css" : {
+      "color" : "rgb(8,88,158)"
+    }
+  }, {
+    "selector" : "node[bipartite = 1]",
+    "css" : {
+      "color" : "rgb(31,161,135)"
+    }
+  }, {
+    "selector" : "node[Degree > 197]",
+    "css" : {
+      "font-size" : 1
+    }
+  }, {
+    "selector" : "node[Degree = 197]",
+    "css" : {
+      "font-size" : 10
+    }
+  }, {
+    "selector" : "node[Degree > 1][Degree < 197]",
+    "css" : {
+      "font-size" : "mapData(Degree,1,197,8,10)"
+    }
+  }, {
+    "selector" : "node[Degree = 1]",
+    "css" : {
+      "font-size" : 8
+    }
+  }, {
+    "selector" : "node[Degree < 1]",
+    "css" : {
+      "font-size" : 1
+    }
+  }, {
+    "selector" : "node[ id = '10328' ]",
+    "css" : {
+      "background-color" : "rgb(0,255,255)",
+      "shape" : "diamond"
+    }
+  }, {
+    "selector" : "node[ id = '10620' ]",
+    "css" : {
+      "background-color" : "rgb(255,223,0)",
+      "shape" : "rectangle"
+    }
+  }, {
+    "selector" : "node[ id = '556297' ]",
+    "css" : {
+      "background-color" : "rgb(255,223,0)",
+      "shape" : "rectangle"
+    }
+  }, {
+    "selector" : "node[ id = '556589' ]",
+    "css" : {
+      "background-color" : "rgb(0,255,255)",
+      "shape" : "diamond"
+    }
+  }, {
+    "selector" : "node[ id = '10328' ]",
+    "css" : {
+      "background-color" : "rgb(0,255,255)",
+      "shape" : "diamond"
+    }
+  }, {
+    "selector" : "node[ id = '10620' ]",
+    "css" : {
+      "background-color" : "rgb(255,223,0)",
+      "shape" : "rectangle"
+    }
+  }, {
+    "selector" : "node:selected",
+    "css" : {
+      "background-color" : "rgb(255,255,0)"
+    }
+  }, {
+    "selector" : "edge",
+    "css" : {
+      "line-color" : "rgb(153,153,153)",
+      "width" : 1.0,
+      "text-opacity" : 1.0,
+      "target-arrow-shape" : "none",
+      "opacity" : 1.0,
+      "target-arrow-color" : "rgb(0,0,0)",
+      "line-style" : "solid",
+      "source-arrow-shape" : "none",
+      "font-family" : "Dialog",
+      "font-weight" : "normal",
+      "color" : "rgb(51,51,51)",
+      "font-size" : 8,
+      "source-arrow-color" : "rgb(0,0,0)",
+      "content" : "data(topic_labels)"
+    }
+  }, {
+    "selector" : "edge[weight > 0.04259112]",
+    "css" : {
+      "font-size" : 1
+    }
+  }, {
+    "selector" : "edge[weight = 0.04259112]",
+    "css" : {
+      "font-size" : 6
+    }
+  }, {
+    "selector" : "edge[weight > 0.02053635][weight < 0.04259112]",
+    "css" : {
+      "font-size" : "mapData(weight,0.02053635,0.04259112,3,6)"
+    }
+  }, {
+    "selector" : "edge[weight = 0.02053635]",
+    "css" : {
+      "font-size" : 3
+    }
+  }, {
+    "selector" : "edge[weight < 0.02053635]",
+    "css" : {
+      "font-size" : 1
+    }
+  }, {
+    "selector" : "edge[weight > 5.56817478]",
+    "css" : {
+      "width" : 1.0
+    }
+  }, {
+    "selector" : "edge[weight = 5.56817478]",
+    "css" : {
+      "width" : 2.0
+    }
+  }, {
+    "selector" : "edge[weight > 0.00025818][weight < 5.56817478]",
+    "css" : {
+      "width" : "mapData(weight,0.00025818,5.56817478,0.13533834586466165,2.0)"
+    }
+  }, {
+    "selector" : "edge[weight = 0.00025818]",
+    "css" : {
+      "width" : 0.13533834586466165
+    }
+  }, {
+    "selector" : "edge[weight < 0.00025818]",
+    "css" : {
+      "width" : 1.0
+    }
+  }, {
+    "selector" : "edge[interaction = 'pp']",
+    "css" : {
+      "line-style" : "solid"
+    }
+  }, {
+    "selector" : "edge[interaction = 'pd']",
+    "css" : {
+      "line-style" : "dashed"
+    }
+  }, {
+    "selector" : "edge[weight = 0.027437503599255394]",
+    "css" : {
+      "color" : "rgb(203,213,232)"
+    }
+  }, {
+    "selector" : "edge[weight = 0.02499709488109897]",
+    "css" : {
+      "color" : "rgb(253,205,172)"
+    }
+  }, {
+    "selector" : "edge[weight = 0.042591117831955766]",
+    "css" : {
+      "color" : "rgb(244,202,228)"
+    }
+  }, {
+    "selector" : "edge[weight = 0.020536352394442913]",
+    "css" : {
+      "color" : "rgb(179,226,205)"
+    }
+  }, {
+    "selector" : "edge:selected",
+    "css" : {
+      "line-color" : "rgb(255,0,0)"
+    }
+  } ]
+} ]
